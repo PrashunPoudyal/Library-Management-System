bf00fafff735805a39d770c573fd8380e288a3c0
prashun-the-baboon <70042744+prashun-the-baboon@users.noreply.github.com>
2021-07-08 15:07:17 -0600

    User Interface changes
    in the client.py file, the main changes have been in the user interface handler and the user interface. There is now a login window. If you wish to login using this you will have to update some code.
    
    For starters you will have to change the I.P. address to the address of the PC that your server is running on. You can also change the port to something you like to use better if you want, although its not needed.
    
    you will not be able to use the functions for sending emails as the email bots password is not shown. Once the finished version is complete all that information will be available.
    
    when you start up the program you will also notice that there are no books yet. that is because I have not added the .dat files.
    
    You will have to add your own books to the program and make sure that it saves to your computer.

--- client.py
+++ client.py
@@ -11,7 +11,10 @@ UserInterface => makes the information look asthetic for the user using the tkin
 
 # imports
 import socket
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from functools import partial
 import pickle
 '''
 This file is the file that is on the user side. User Class will still be on library side. It is the userInterface that the
@@ -37,162 +40,286 @@ ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def send(msg):
-    message = msg.encode('utf-8')
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-
-def serverRequestUserSignIn(userID, password):
-    # message for function sign in
-    message = b'requestSignIn'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    # message for user
-    userIDmessage = userID.encode('utf-8')
-    msgLength = len(userIDmessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(userIDmessage)
-    # message for password
-    passwordMessage = password.encode('utf-8')
-    msgLength = len(passwordMessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(passwordMessage)
-
-def serverRequestUserSignOut(userID):
-    # message for function sign in
-    message = b'requestSignOut'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    # message for user
-    userIDmessage = userID.encode('utf-8')
-    msgLength = len(userIDmessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(userIDmessage)
-
-def adminRequestDeleteUser(userID, admPassword):
-    # message for function sign in
-    message = b'requestDeleteUser'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    # message for user
-    userIDmessage = userID.encode('utf-8')
-    msgLength = len(userIDmessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(userIDmessage)
-    # message for user
-    password = admPassword.encode('utf-8')
-    msgLength = len(password)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(password)
-
-def adminRequestDeleteBook(userID, admPassword):
-    # message for function sign in
-    message = b'requestDeleteBook'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    # message for user
-    userIDmessage = userID.encode('utf-8')
-    msgLength = len(userIDmessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(userIDmessage)
-    # message for user
-    password = admPassword.encode('utf-8')
-    msgLength = len(password)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(password)
-
-def serverRequestUserInformation(userID):
-    # message for function sign in
-    message = b'requestUserInformation'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    # message for user
-    userIDmessage = userID.encode('utf-8')
-    msgLength = len(userIDmessage)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(userIDmessage)
-    returnedmsgLength = client.recv(HEADER).decode('utf-8')
-    if returnedmsgLength:
-        returnedmsgLength = int(returnedmsgLength)
-        userInformation = client.recv(returnedmsgLength)
-        userInformation = pickle.loads(userInformation)
-        return userInformation
-
-def serverRequestShowBooks():
-    message = b'showBooksForBrowse'
-    msgLength = len(message)
-    sendLength = str(msgLength).encode('utf-8')
-    sendLength += b' ' * (HEADER - len(sendLength))
-    client.send(sendLength)
-    client.send(message)
-    msgLength = client.recv(HEADER).decode('utf-8')
-    if msgLength:
-        msgLength = int(msgLength)
-        booksList = client.recv(msgLength)
-        booksList = pickle.loads(booksList)
-        return booksList
+'''
+There are two classes in the client.py file.
 
 
-def connect():
-    try:
-        client.connect(ADDR)
-    except ConnectionRefusedError:
-        print("SERVER COULD NOT CONNECT TO CLIENT - EITHER YOU ARE AN INVALID CONNECTION OR THE SERVER IS DOWN")
-    except ConnectionError:
-        print("ConnectionError - something went wrong, please try again")
-    except:
-        print("Unknown Error - please try again")
+first:
+UserInterfaceHandler.
 
-    return True
+this class is used specifically for communicating with the UI, and with the server. It converts the information
+from the server to the UI, or converts the information from the UI to give to the server.
 
-def main():
-    print("main has started")
-    send("HELLO THERE")
+essentially, it goes from computer readable information to user readable information, and is the midddleman
+for the user side of communication.
 
-def disconnect():
-    send(DISCONNECT_MESSAGE)
 
-connect()
+second:
+UserInterface
 
-x = input('enter: ')
+the UI class does not yet exist, but it is the class that displays information that looks good.
 
-booksList = serverRequestShowBooks()
+as of now it does not have anything other than log in and log out. Information about the UI design is available
+on github as well.
+'''
+class UserInterfaceHandler:
+    def __init__(self):
+        self.name = 'userInterfaceHandler'
 
-for i in range(len(booksList)):
-    print(f"{booksList[i]}\n")
+    def send(self, msg):
+        message = msg.encode('utf-8')
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
 
-disconnect()
+    def leaveConnection(self, userID):
+        self.serverRequestUserSignOut(userID)
+        self.disconnect()
+
+    def serverRequestLibraryName(self):
+        # message for function name
+        message = b'requestLibraryName'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        returnedmsgLength = client.recv(HEADER).decode('utf-8')
+        if returnedmsgLength:
+            returnedmsgLength = int(returnedmsgLength)
+            message = client.recv(returnedmsgLength)
+            message = message.decode('utf-8')
+            return message
+
+    def serverRequestUserSignIn(self, userID, password):
+        # message for function sign in
+        message = b'requestSignIn'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        userIDmessage = userID.encode('utf-8')
+        msgLength = len(userIDmessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(userIDmessage)
+        # message for password
+        passwordMessage = password.encode('utf-8')
+        msgLength = len(passwordMessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(passwordMessage)
+        # wait for return message
+        returnedmsgLength = client.recv(HEADER).decode('utf-8')
+        if returnedmsgLength:
+            returnedmsgLength = int(returnedmsgLength)
+            confirm = client.recv(returnedmsgLength).decode('utf-8')
+            return confirm
+
+    def serverRequestUserSignOut(self, userID):
+        # message for function sign in
+        message = b'requestSignOut'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        userIDmessage = userID.encode('utf-8')
+        msgLength = len(userIDmessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(userIDmessage)
+
+    def adminRequestDeleteUser(self, userID, admPassword):
+        # message for function sign in
+        message = b'requestDeleteUser'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        userIDmessage = userID.encode('utf-8')
+        msgLength = len(userIDmessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(userIDmessage)
+        # message for user
+        password = admPassword.encode('utf-8')
+        msgLength = len(password)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(password)
+
+    def adminRequestDeleteBook(self, userID, admPassword):
+        # message for function sign in
+        message = b'requestDeleteBook'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        userIDmessage = userID.encode('utf-8')
+        msgLength = len(userIDmessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(userIDmessage)
+        # message for user
+        password = admPassword.encode('utf-8')
+        msgLength = len(password)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(password)
+
+    def serverRequestUserInformation(self, userID):
+        # message for function sign in
+        message = b'requestUserInformation'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        # message for user
+        userIDmessage = userID.encode('utf-8')
+        msgLength = len(userIDmessage)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(userIDmessage)
+        returnedmsgLength = client.recv(HEADER).decode('utf-8')
+        if returnedmsgLength:
+            try:
+                returnedmsgLength = int(returnedmsgLength)
+                userInformation = client.recv(returnedmsgLength)
+                userInformation = pickle.loads(userInformation)
+                return userInformation
+            except:
+                return None
+
+    def serverRequestShowBooks(self):
+        message = b'showBooksForBrowse'
+        msgLength = len(message)
+        sendLength = str(msgLength).encode('utf-8')
+        sendLength += b' ' * (HEADER - len(sendLength))
+        client.send(sendLength)
+        client.send(message)
+        msgLength = client.recv(HEADER).decode('utf-8')
+        if msgLength:
+            msgLength = int(msgLength)
+            booksList = client.recv(msgLength)
+            booksList = pickle.loads(booksList)
+            return booksList
+
+    def connect(self):
+        try:
+            client.connect(ADDR)
+        except ConnectionRefusedError:
+            print("SERVER COULD NOT CONNECT TO CLIENT - EITHER YOU ARE AN INVALID CONNECTION OR THE SERVER IS DOWN")
+        except ConnectionError:
+            print("ConnectionError - something went wrong, please try again")
+        except:
+            print("Unknown Error - please try again")
+
+        return True
+
+    def main(self):
+        print("main has started")
+
+    def disconnect(self):
+        self.send(DISCONNECT_MESSAGE)
+
+uiHandler = UserInterfaceHandler()
+
+uiHandler.connect()
+
+def userSettingsPage(username):
+    def back():
+        homePage()
+
+    settingsPage = Tk()
+
+
+def homePage(username):
+    def logoutandexit():
+        if messagebox.askokcancel("ARE YOU SURE?", "ARE YOU SURE YOU WANT TO QUIT PROGRAM?"):
+            uiHandler.serverRequestUserSignOut(username)
+            uiHandler.disconnect()
+            quit()
+
+    libraryName = uiHandler.serverRequestLibraryName()
+    homeWindow = Tk()
+    homeWindow.geometry('600x450')
+    userInformation = uiHandler.serverRequestUserInformation(username)
+    print(userInformation)
+    homeWindow.title(f"{libraryName} User Home Page - {username}")
+
+    tabControl = ttk.Notebook(homeWindow)
+    tab1 = ttk.Frame(tabControl)
+    tab2 = ttk.Frame(tabControl)
+    tab3 = ttk.Frame(tabControl)
+    tabControl.add(tab1, text='Home Page')
+    tabControl.add(tab2, text='Browse Books')
+    tabControl.add(tab3, text='Settings')
+    tabControl.pack(expand=1, fill="both")
+
+    ttk.Label(tab1, text=f"{userInformation['name']}").grid(row=0, column=0)
+    btn = ttk.Button(tab1, text='Log Out & Exit', command=logoutandexit).grid(row=10, column=10)
+
+    homeWindow.mainloop()
+
+
+def loginPage():
+    def validateLogin(username, password):
+        print("validating login...")
+        confirm = uiHandler.serverRequestUserSignIn(username.get(), password.get())
+        if confirm == 'True':
+            print("USER SIGNED IN")
+            tkWindow.destroy()
+            homePage(username.get())
+        else:
+            print(confirm)
+            print("USER PASSWORD OR USERNAME IS INCORRECT")
+
+    #window
+    libraryName = uiHandler.serverRequestLibraryName()
+
+    tkWindow = Tk()
+    tkWindow.geometry('400x150')
+    tkWindow.title(f"{libraryName} Login Page")
+
+    #username label and text entry box
+    usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+    username = StringVar()
+    usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
+
+    #password label and password entry box
+    passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)
+    password = StringVar()
+    passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+
+    validateLogin = partial(validateLogin, username, password)
+
+    #login button
+    loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)
+
+    tkWindow.mainloop()
+
+uiHandler.serverRequestUserSignOut('43103')
+
+loginPage()
+
+uiHandler.disconnect()
